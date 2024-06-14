195a77665f867077ae42becd0333f668c65f971a
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-06-15 00:18:40 +0200

    Inici del projecte, creació del spring

---
+++ TerraneanSound/src/test/java/terranean/sound/TerraneanSound/TerraneanSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package terranean.sound.TerraneanSound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraneanSoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

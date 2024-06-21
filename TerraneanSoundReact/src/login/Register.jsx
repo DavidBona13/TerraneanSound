d7cb61535f887f3da52f0ce5e3a9775eca2240f8
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-06-21 17:56:45 +0200

    prueba

---
+++ TerraneanSoundReact/src/login/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../assets/css/register.css';
+
+const Register = () => {    
+
+    return (
+        <div className='containerRegistro'>
+            <section className='section'>
+                <h1 className='registro'>Regístrate</h1>
+                <form>
+
+                </form>
+            </section>
+        </div>
+    )
+}
+
+export default Register;
